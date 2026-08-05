82b0bc3b0dac6ba2567506095bdd29133b4020d5
HoangTuGio <khangdeptrai198@gmail.com>
2026-08-05 21:30:48 +0700

    Add React Native web preview

---
+++ webpack.config.js
@@ -0,0 +1,67 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+const appDirectory = __dirname;
+
+module.exports = {
+  mode: process.env.NODE_ENV === 'production' ? 'production' : 'development',
+  entry: path.resolve(appDirectory, 'index.web.js'),
+  output: {
+    clean: true,
+    filename: 'bundle.web.js',
+    path: path.resolve(appDirectory, 'dist-web'),
+    publicPath: '/',
+  },
+  devtool: 'source-map',
+  devServer: {
+    historyApiFallback: true,
+    hot: true,
+    open: true,
+    port: 3000,
+    static: {
+      directory: path.resolve(appDirectory, 'public'),
+    },
+  },
+  module: {
+    rules: [
+      {
+        test: /\.[jt]sx?$/,
+        include: [
+          path.resolve(appDirectory, 'App.tsx'),
+          path.resolve(appDirectory, 'index.web.js'),
+          path.resolve(
+            appDirectory,
+            'node_modules/react-native-safe-area-context',
+          ),
+        ],
+        use: {
+          loader: 'babel-loader',
+          options: {
+            cacheDirectory: true,
+            presets: ['module:@react-native/babel-preset'],
+          },
+        },
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.resolve(appDirectory, 'public/index.html'),
+    }),
+  ],
+  resolve: {
+    alias: {
+      'react-native$': 'react-native-web',
+    },
+    extensions: [
+      '.web.tsx',
+      '.web.ts',
+      '.tsx',
+      '.ts',
+      '.web.jsx',
+      '.web.js',
+      '.jsx',
+      '.js',
+    ],
+  },
+};
